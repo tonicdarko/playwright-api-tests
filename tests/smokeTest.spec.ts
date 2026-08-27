8c755ce5b9e621e7fb5793ed323fa6429dc0cd96
Darko Tonic <tonicdarko@yahoo.com>
2026-08-27 22:52:40 +0200

    working flow

--- tests/smokeTest.spec.ts
+++ tests/smokeTest.spec.ts
@@ -1,22 +1,62 @@
-import { test } from '../utils/fixtures';
-import { expect } from '@playwright/test';
+import { test } from "../utils/fixtures";
+import { expect } from "@playwright/test";
 
-test('Get Articles', async({ api }) => {
-    
- const response = await api
- .path('/articles')
- .params({limit:10, offset:0})
- .getRequest(200)
+let authToken: string;
 
- expect(response.articles.length).toBeLessThanOrEqual(10);
- expect(response.articlesCount).toBeLessThanOrEqual(10);
+test.beforeAll("Get Auth Token", async ({ api }) => {
+  const tokenResponse = await api
+    .path("/users/login")
+    .body({ email: "testingudemy@testing.com", password: "Brljotina12" })
+    .postRequest(200);
+
+  authToken = "Token " + tokenResponse.user.token;
 });
 
-test('Get Tags', async({ api }) =>{
-    const response = await api
-    .path('/tags')
-    .getRequest(200)
+test("Get Articles", async ({ api }) => {
+  const response = await api
+    .path("/articles")
+    .params({ limit: 10, offset: 0 })
+    .getRequest(200);
 
-    expect(response.tags[0]).toEqual('Test');
-    expect(response.tags.length).toBeLessThanOrEqual(10)
-})
+  expect(response.articles.length).toBeLessThanOrEqual(10);
+  expect(response.articlesCount).toBeLessThanOrEqual(10);
+});
+
+test("Get Tags", async ({ api }) => {
+  const response = await api.path("/tags").getRequest(200);
+
+  expect(response.tags[0]).toEqual("Test");
+  expect(response.tags.length).toBeLessThanOrEqual(10);
+});
+
+test("Create and delete Article", async ({ api }) => {
+  const createArticleResponse = await api
+    .path("/articles")
+    .headers({ Authorization: authToken })
+    .body({
+      article: {
+        title: "Playwright Test",
+        description: "Testing Udemy",
+        body: "Testing body",
+        tagList: [],
+      },
+    })
+    .postRequest(201);
+
+  expect(createArticleResponse.article.title).toEqual("Playwright Test");
+
+  const slugId = createArticleResponse.article.slug;
+
+  const articlesResponse = await api
+    .path("/articles")
+    .headers({ Authorization: authToken })
+    .params({ limit: 10, offset: 0 })
+    .getRequest(200);
+
+  expect(articlesResponse.articles[0].title).toEqual("Playwright Test");
+
+  await api
+    .path(`/articles/${slugId}`)
+    .headers({ Authorization: authToken })
+    .deleteRequest(204);
+});
--- utils/request-handler.ts
+++ utils/request-handler.ts
@@ -1,63 +1,98 @@
 import { APIRequestContext } from "@playwright/test";
-import { expect } from '@playwright/test';
+import { expect } from "@playwright/test";
 
-export class RequestHandler{
-    
-    private request: APIRequestContext;
-    private baseUrl: string = 'https://conduit-api.bondaracademy.com/api';
-    private defaultBaseUrl: string = '';
-    private apiPath: string = '';
-    private queryParams: object = {};
-    private apiHeaders: Record<string, string> = {};
-    private apiBody: object = {};
+export class RequestHandler {
+  private request: APIRequestContext;
+  private baseUrl: string = "https://conduit-api.bondaracademy.com/api";
+  private defaultBaseUrl: string = "";
+  private apiPath: string = "";
+  private queryParams: object = {};
+  private apiHeaders: Record<string, string> = {};
+  private apiBody: object = {};
 
-    constructor(request: APIRequestContext, apiBaseUrl: string){
-        this.request = request;
-        this.defaultBaseUrl = apiBaseUrl;
+  constructor(request: APIRequestContext, apiBaseUrl: string) {
+    this.request = request;
+    this.defaultBaseUrl = apiBaseUrl;
+  }
+
+  url(url: string) {
+    this.baseUrl = url;
+    return this;
+  }
+
+  path(path: string) {
+    this.apiPath = path;
+    return this;
+  }
+
+  params(params: object) {
+    this.queryParams = params;
+    return this;
+  }
+
+  headers(headers: Record<string, string>) {
+    this.apiHeaders = headers;
+    return this;
+  }
+
+  body(body: object) {
+    this.apiBody = body;
+    return this;
+  }
+
+  async getRequest(statusCode: number) {
+    const url = this.getUrl();
+    const response = await this.request.get(url, {
+      headers: this.apiHeaders,
+    });
+    const responseJson = await response.json();
+
+    expect(response.status()).toEqual(statusCode);
+
+    return responseJson;
+  }
+
+  async postRequest(statusCode: number) {
+    const url = this.getUrl();
+    const response = await this.request.post(url, {
+      headers: this.apiHeaders,
+      data: this.apiBody,
+    });
+    const responseJson = await response.json();
+
+    expect(response.status()).toEqual(statusCode);
+
+    return responseJson;
+  }
+
+  async putRequest(statusCode: number) {
+    const url = this.getUrl();
+    const response = await this.request.put(url, {
+      headers: this.apiHeaders,
+      data: this.apiBody,
+    });
+    const responseJson = await response.json();
+
+    expect(response.status()).toEqual(statusCode);
+
+    return responseJson;
+  }
+
+  async deleteRequest(statusCode: number) {
+    const url = this.getUrl();
+    const response = await this.request.delete(url, {
+      headers: this.apiHeaders,
+    });
+    expect(response.status()).toEqual(statusCode);
+  }
+
+  private getUrl() {
+    const url = new URL(
+      `${this.baseUrl ?? this.defaultBaseUrl}${this.apiPath}`,
+    );
+    for (const [key, value] of Object.entries(this.queryParams)) {
+      url.searchParams.append(key, value);
     }
-
-    url(url: string){
-        this.baseUrl = url;
-        return this;
-    }
-
-    path(path: string){
-        this.apiPath = path;
-        return this;
-    }
-
-    params(params: object){
-        this.queryParams = params;
-        return this;
-    }
-
-    headers(headers: Record<string, string>){
-        this.apiHeaders = headers;
-        return this;
-    }
-
-    body(body: object){
-        this.apiBody = body;
-        return this;
-    }
-
-    async getRequest(statusCode: number){
-        const url = this.getUrl();
-        const response = await this.request.get(url, {
-            headers: this.apiHeaders
-        })
-        const responseJson = await response.json();
-
-        expect(response.status()).toEqual(statusCode)
-
-        return responseJson;
-    }
-
-    private getUrl(){
-        const url = new URL(`${this.baseUrl ?? this.defaultBaseUrl}${this.apiPath}`);
-        for( const [key, value] of Object.entries(this.queryParams)){
-            url.searchParams.append(key, value)
-        }
-        return url.toString();
-    }
+    return url.toString();
+  }
 }
